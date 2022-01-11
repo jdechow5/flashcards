a9e4e374e430785a6621d291112a70d5732f27b2
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-01-11 13:38:31 -0800

    add six card divs, make them flippable
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  $("#first-head").click(function() {
+    $("#first-card").toggle();
+    $("#first-head").toggle();
+  });
+
+  $("#first-card").click(function() {
+    $("#first-card").toggle();
+    $("#first-head").toggle();
+  });
+
+  $("#second-head").click(function() {
+    $("#second-card").toggle();
+  });
+
+  $("#third-head").click(function() {
+    $("#third-card").toggle();
+  });
+
+  $("#fourth-head").click(function() {
+    $("#fourth-card").toggle();
+  });
+  
+  $("#fifth-head").click(function() {
+    $("#fifth-card").toggle();
+  });
+
+  $("#sixth-head").click(function() {
+    $("#sixth-card").toggle();
+  });
+
+});
